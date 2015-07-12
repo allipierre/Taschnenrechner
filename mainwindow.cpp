c46182d93a9a695c94498630127b20f205fa447c
allipierre <yottiallipierre40@yahoo.fr>
2015-07-12 14:27:20 -0700

    neue

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,68 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+
+mywidget=new QWidget();
+    outputNummer=new  QLineEdit();
+   num9=new   QPushButton("9");
+    num8=new   QPushButton("8");
+     num7=new   QPushButton("7");
+      num6=new   QPushButton("6");
+       num5=new   QPushButton("5");
+        num4=new   QPushButton("4");
+         num3=new   QPushButton("3");
+          num2=new   QPushButton("2");
+           num1=new   QPushButton("1");
+            num0=new   QPushButton("0");
+
+                       point=new QPushButton(".") ;
+                          equal=new QPushButton("=");
+                           opAdd=new QPushButton ("+");
+                            opMinus=new QPushButton("-");
+                              opMal=new QPushButton("*");
+                              opDiv=new QPushButton("/");
+                              opDel=new QPushButton("AC");
+
+     qvboxl=new QVBoxLayout();
+
+
+
+       hlayout1=new QHBoxLayout();
+        this->hlayout1->addWidget(this->outputNummer);
+        this->hlayout1->addWidget(this->opDel);
+        this->qvboxl->addLayout(this->hlayout1);
+
+        hlayout2=new QHBoxLayout();
+        this->hlayout2->addWidget(this->num7);
+        this->hlayout2->addWidget(this->num8);
+        this->hlayout2->addWidget(this->num9);
+        this->hlayout2->addWidget(this->opAdd);
+        this->qvboxl->addLayout(this->hlayout2);
+
+
+       hlayout3=new QHBoxLayout();
+       this->hlayout3->addWidget(this->num4);
+       this->hlayout3->addWidget(this->num5);
+       this->hlayout3->addWidget(this->num6);
+       this->hlayout3->addWidget(this->opMinus);
+       this->qvboxl->addLayout(this->hlayout3);
+
+       hlayout4=new QHBoxLayout();
+       this->hlayout4->addWidget(this->num1);
+       this->hlayout4->addWidget(this->num2);
+       this->hlayout4->addWidget(this->num3);
+       this->hlayout4->addWidget(this->opMal);
+       this->qvboxl->addLayout(this->hlayout4);
+
+       hlayout5=new QHBoxLayout();
+       this->hlayout5->addWidget(this->num0);
+       this->hlayout5->addWidget(this->point);
+       this->hlayout5->addWidget(this->equal);
+       this->hlayout5->addWidget(this->opDiv);
+       this->qvboxl->addLayout(this->hlayout5);
+
+       mywidget->setLayout(this->qvboxl);
+       this->setCentralWidget(mywidget);
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include<QLabel>
+#include<QLayout>
+#include<QPushButton>
+#include<QLineEdit>
 
 class MainWindow : public QMainWindow
 {
@@ -10,6 +14,35 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    QWidget *mywidget;
+    QLineEdit *outputNummer;
+    QPushButton *num9;
+      QPushButton *num8;
+        QPushButton *num7;
+          QPushButton *num6;
+            QPushButton *num5;
+              QPushButton *num4;
+                QPushButton *num3;
+                  QPushButton *num2;
+                    QPushButton *num1;
+                      QPushButton *num0;
+                        QPushButton *point;
+                          QPushButton *equal;
+                            QPushButton *opAdd;
+                             QPushButton *opMinus;
+                              QPushButton *opMal;
+                               QPushButton *opDiv;
+                                QPushButton *opDel;
+
+      QVBoxLayout *qvboxl;
+
+      //Haben wir 5 Ebene, deswegen 5 Horizontale layout
+      QHBoxLayout * hlayout1;
+      QHBoxLayout * hlayout2;
+      QHBoxLayout * hlayout3;
+      QHBoxLayout * hlayout4;
+      QHBoxLayout * hlayout5;
+
 };
 
 #endif // MAINWINDOW_H
